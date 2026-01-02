a953636eb889244cf0503fa25f525b74f1a0aca6
liming <lim227417@gmail.com>
2026-01-02 10:30:44 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ryan",
+    pageTitle: "ZenGen",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "lim227417-tech.github.io/obsidian--publish",
+    baseUrl: "zengen.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
