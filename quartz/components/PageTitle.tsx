12469a0d2c4f895613ad471994102f235f4d5307
liming <lim227417@gmail.com>
2026-02-21 09:38:18 +0800

    feat: add logo to page title and reduce top spacing

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -2,13 +2,18 @@ import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
+import { joinSegments } from "../util/path"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const logoPath = joinSegments(baseDir, "static/logo.png")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir}>
+        <img src={logoPath} alt="logo" class="page-title-logo" />
+        {title}
+      </a>
     </h2>
   )
 }
@@ -18,6 +23,18 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+
+.page-title a {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.page-title-logo {
+  height: 3rem;
+  width: auto;
+  object-fit: contain;
 }
 `
 
